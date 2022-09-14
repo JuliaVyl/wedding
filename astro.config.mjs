4893a8f6619e62ff360e3f007ee13ca39e46c17a
Andrey Gordeev <prolodgy1997@gmail.com>
2022-09-14 15:03:15 +0300

    Add vercel support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
-export default {
+// https://astro.build/config
+export default defineConfig({
   // ...
   integrations: [react()],
-}
+  output: "server",
+  adapter: vercel()
+});
