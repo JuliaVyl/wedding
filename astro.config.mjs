2932e2ee15671d02ad350b35c8fe32b358e98ea2
Andrey Gordeev <prolodgy1997@gmail.com>
2022-09-19 10:20:44 +0300

    Remove @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
-import image from '@astrojs/image';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image()],
+  integrations: [react()],
   output: 'server',
   adapter: vercel(),
   define: {
